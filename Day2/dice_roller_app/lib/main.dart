fa9638bb43869d22351c09d7ce862b0fa1ab58d8
Divyansh <divyanshraj0408@gmail.com>
2024-12-31 10:23:36 +0530

    initial commit

---
+++ Day2/dice_roller_app/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:dice_roller_app/dice_roller.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Dice Roller App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const DiceRoller(),
+    );
+  }
+}
